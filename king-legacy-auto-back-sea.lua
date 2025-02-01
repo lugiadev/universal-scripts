cf63b1ac048cc1f15bcb96eb8134f129158fbef3
lugia <natsurblx@gmail.com>
2025-02-01 15:11:50 +0700

    Update king-legacy-auto-back-sea.lua

--- king-legacy-auto-back-sea.lua
+++ king-legacy-auto-back-sea.lua
@@ -1,3 +1,7 @@
+if game.PlaceId ~= 15759515082 then
+	return
+end
+
 repeat
 	task.wait()
 until game:IsLoaded() and game.Players.LocalPlayer
@@ -12,7 +16,7 @@ function CurrentDialog()
 			continue
 		end
 		if gui:FindFirstChild("Dialogue") and gui.Dialogue.Visible then
-			print(gui.Name)
+			print("Found " .. gui.Name)
 			return gui
 		end
 	end
@@ -29,13 +33,21 @@ function AcceptDialog(name)
 		return
 	end
 
-	print(Dialog.Name)
-
-	local Accept = Dialog.Dialogue:FindFirstChild("Accept")
+	local Dialogue = Dialog:FindFirstChild("Dialogue")
+	local Accept = Dialogue and Dialogue:FindFirstChild("Accept")
 	if Accept then
-		print(Accept.Name)
-		for _, connection in pairs(getconnections(Accept.MouseButton1Click)) do
-			connection:Fire()
+		Dialog.DisplayOrder = 99999999
+
+		local position = Accept.AbsolutePosition
+        local size = Accept.AbsoluteSize
+        local clickX = position.X + (size.X / 2)
+        local clickY = position.Y + (size.Y / 2)
+        VirtualUser:Button1Down(Vector2.new(clickX, clickY), workspace.CurrentCamera.CFrame)
+        task.wait(0.1)
+        VirtualUser:Button1Up(Vector2.new(clickX, clickY), workspace.CurrentCamera.CFrame)
+
+		for i, v in pairs(getconnections(Accept.MouseButton1Click)) do
+			v:Fire()
 		end
 	end
 end
@@ -49,14 +61,14 @@ end)
 
 -- Start the script
 local function main()
-	local NPCs = workspace:WaitForChild("AllNPC", 10) -- Added timeout
+	local NPCs = workspace:FindFirstChild("AllNPC", 9e9) -- Added timeout
 	print(NPCs)
 	if not NPCs then
 		warn("Failed to find AllNPC folder")
 		return
 	end
 
-	if not plr.PlayerStats or not plr.PlayerStats:FindFirstChild("SecondSeaProgression") then
+	if not plr.PlayerStats or not plr.PlayerStats:FindFirstChild("SecondSeaProgression", 9e9) then
 		warn("PlayerStats or SecondSeaProgression not found")
 		return
 	end
@@ -66,23 +78,23 @@ local function main()
 	until plr.PlayerStats.SecondSeaProgression.Value == "Yes"
 
 	while true do
-		if not plr.Character or not plr.Character:FindFirstChild("HumanoidRootPart") then
+		if not plr.Character or not plr.Character:FindFirstChild("HumanoidRootPart", 9e9) then
 			task.wait(1)
 			continue
 		end
 
-		local elitePirate = NPCs:FindFirstChild("Elite Pirate")
+		local elitePirate = NPCs:FindFirstChild("The Squid", 9e9)
 		if not elitePirate then
 			task.wait(1)
 			continue
 		end
 
-		plr.Character.HumanoidRootPart.CFrame = elitePirate.CFrame * CFrame.new(0, 0, -4)
+		plr.Character.HumanoidRootPart.CFrame = elitePirate.CFrame * CFrame.new(0, 0, -3)
 		local _ = Remotes.Functions.CheckQuest:InvokeServer(elitePirate)
-		task.wait(1)
-		AcceptDialog("Elite Pirate")
+		task.wait(2)
+		AcceptDialog("The Squid")
 
-		task.wait(1) -- Prevent infinite loop from consuming too much CPU
+		task.wait(2) -- Prevent infinite loop from consuming too much CPU
 	end
 end
 task.spawn(main)
